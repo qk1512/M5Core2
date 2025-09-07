0e2f9d095e4f5ab52555343599638d8a5fa23a32
khanh <doquangkhanh2@gmail.com>
2025-09-07 14:35:07 +0700

    update first version

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,8 @@ AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
 DHT20 dht20;
 
-#define SDA_PIN 11
-#define SCL_PIN 12
+#define SDA_PIN 32
+#define SCL_PIN 33
 
 void notifyClients(float temp, float hum)
 {
@@ -27,16 +27,36 @@ void notifyClients(float temp, float hum)
   ws.textAll(jsonStr);
 }
 
+float tempOffset = 0.0;
+float humOffset = 0.0;
+
+void updateLCD(float tempOffset, float humOffset)
+{
+  M5.Lcd.fillRect(0, 26, 240, 96, BLACK); // clear old values
+  M5.Lcd.setCursor(0, 30);
+  M5.Lcd.printf("Temp cal : (%.2f)\n", tempOffset);
+  M5.Lcd.printf("Hum  cal : (%.2f)\n", humOffset);
+}
+
 void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,
                AwsEventType type, void *arg, uint8_t *data, size_t len)
 {
-  if (type == WS_EVT_CONNECT)
+  if (type == WS_EVT_DATA)
   {
-    Serial.println("WebSocket connected");
-  }
-  else if (type == WS_EVT_DISCONNECT)
-  {
-    Serial.println("WebSocket disconnected");
+    AwsFrameInfo *info = (AwsFrameInfo *)arg;
+    if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT)
+    {
+      data[len] = 0; // Ensure null-terminated
+      StaticJsonDocument<128> doc;
+      DeserializationError err = deserializeJson(doc, (char *)data);
+      if (!err && doc["type"] == "calibrate")
+      {
+        tempOffset = doc["tempOffset"] | 0.0;
+        humOffset = doc["humOffset"] | 0.0;
+        Serial.printf("Calibration updated: tempOffset=%.2f, humOffset=%.2f\n", tempOffset, humOffset);
+        //updateLCD(tempOffset, humOffset);
+      }
+    }
   }
 }
 
@@ -67,14 +87,16 @@ void setupWebServer()
 
 void setup()
 {
-  M5.begin(115200);
+  M5.begin(true, false,true,true,kMBusModeOutput,false);
   Serial.begin(115200);
+  Wire.begin(SDA_PIN, SCL_PIN);
+
 
   M5.Lcd.setTextColor(WHITE);
   M5.Lcd.setTextSize(2);
   M5.Lcd.println("Starting WebSocket...");
 
-  //SPIFFS.begin(true);
+  SPIFFS.begin(true);
   setupWiFi();
 
   //dht20.begin(Wire); // Default SDA/SCL
@@ -82,20 +104,20 @@ void setup()
   setupWebServer();
 }
 
+
+
 void loop()
 {
   static unsigned long lastSend = 0;
   if (millis() - lastSend > 2000)
   {
     lastSend = millis();
+    dht20.read();
 
     float temp = dht20.getTemperature();
     float hum = dht20.getHumidity();
 
-    M5.Lcd.setCursor(0, 30);
-    M5.Lcd.printf("Temp: %.1f C\n", temp);
-    M5.Lcd.printf("Hum:  %.1f %%\n", hum);
-
+    updateLCD(tempOffset, humOffset);
     notifyClients(temp, hum);
   }
 }
